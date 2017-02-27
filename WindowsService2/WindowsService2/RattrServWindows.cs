0dcbd514ba624e9ef0eb89fbaf12aa1b4d4daeac
Prototype <DylanCa@users.noreply.github.com>
2017-02-27 16:29:32 +0100

    Windows Service
    Creates an event log called "MyNewLog" and adds an event "coucou" every predefined time.

---
+++ WindowsService2/WindowsService2/RattrServWindows.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WindowsService2
+{
+    public partial class RattrServWindows : ServiceBase
+    {
+        private System.Timers.Timer timer;
+
+        public RattrServWindows()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+
+            timer = new System.Timers.Timer();
+            timer.Interval = 6000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Coucou");
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry("Coucou");
+        }
+    }
+}
